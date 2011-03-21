f04f310cddf2d2f8e2819b6e62d9a77a35e7df84
Jon Parise <jon@indelible.org>
2011-03-20 21:46:49 -0700

    Renaming the script to not include the .py suffix.

--- setup.py
+++ setup.py
@@ -2,16 +2,14 @@
 
 from distutils.core import setup
 
-version = __import__('stale').__version__
-
 setup(
     name="stale",
-    version=version,
+    version='1.1',
     description="Identifies (and optionally removes) stale Delicious and Pinboard links",
     author="Jon Parise",
     author_email="jon@indelible.org",
     url="https://github.com/jparise/stale",
-    scripts = ['stale.py'],
+    scripts = ['stale'],
     license = "MIT License",
     classifiers = ['License :: OSI Approved :: MIT License',
                    'Operating System :: OS Independent',
