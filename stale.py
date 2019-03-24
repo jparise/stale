6f550ff64d7866d50831f6ec8423b8a64359c892
Jon Parise <jon@indelible.org>
2019-03-24 15:59:03 -0700

    Bump license years to include 2019

--- stale.py
+++ stale.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2010-2018 Jon Parise <jon@indelible.org>
+# Copyright (c) 2010-2019 Jon Parise <jon@indelible.org>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
